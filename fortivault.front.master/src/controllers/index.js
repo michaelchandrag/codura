bf9c110f6a42a4412bfbb043e5a81fc9c44574dc
Michael Chandra <canzinzzzide@yahoo.co.id>
2025-01-12 21:10:23 +0700

    update

--- fortivault.front.master/src/controllers/index.js
+++ fortivault.front.master/src/controllers/index.js
@@ -1,4 +1,5 @@
 import { fetchData, sendMessageToOpenAI } from '@/configs/api'
+import { sleep, showAlert } from '@/helpers';
 
 export const postMessage = async (message) => {
     const result = await sendMessageToOpenAI(message);
@@ -6,3 +7,15 @@ export const postMessage = async (message) => {
         return result;
     }
 }
+
+export const copyToClipboard = async (value) => {
+    try {
+        await navigator.clipboard.writeText(value);
+        console.log('Text copied to clipboard:', value);
+        showAlert({ type: 'success', text: 'Success Copy CA' });
+        return true;
+    } catch (error) {
+        console.error('Failed to copy text to clipboard:', error);
+        return false;
+    }
+};
--- fortivault.front.master/src/views/Home/Index.vue
+++ fortivault.front.master/src/views/Home/Index.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { onMounted, ref, reactive } from 'vue'
+import { copyToClipboard } from '@/controllers';
 
 let prompText = ref('')
 onMounted(() => {
 
 })
+let addressToCopy = ref('9kEcnFj9vwShFtjyLNULGFugBS9KrYAazMAcx3GWpum')
 </script>
 
 <template>
@@ -19,43 +21,45 @@ onMounted(() => {
             technology. We aim to prevent exploits and promote trust in decentralized systems</p>
         </div>
         <div class="hero-text mb-5">
-          <h2 class="ls-sm fw-bold text-white fs-40px">Get Started (BETA Version)</h2>
-
-        <div class="row gy-4 justify-content-center" data-aos="fade-right" data-aos-delay="100">
-          <div class="col-lg-6">
-            <div class="card rounded-sm feature-card h-100">
-              <div class="card-body text-start">
-                <h6 class="text-white">1. Sign Up</h6>
-                <p class="text-light fw-normal fs-13px m-0">Create your account and connect your Web3 wallet.</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-6">
-            <div class="card rounded-sm feature-card h-100">
-              <div class="card-body text-start">
-                <h6 class="text-white">2. Run a Basic Scan through our AI chat box</h6>
-                <p class="text-light fw-normal fs-13px m-0">Test your smart contract for free.</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-6">
-            <div class="card rounded-sm feature-card h-100">
-              <div class="card-body text-start">
-                <h6 class="text-white">3. Unlock Advanced Features</h6>
-                <p class="text-light fw-normal fs-13px m-0">Stake your tokens for premium audits.</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-6">
-            <div class="card rounded-sm feature-card h-100">
-              <div class="card-body text-start">
-                <h6 class="text-white">4. Receive Security Reports</h6>
-                <p class="text-light fw-normal fs-13px m-0">Get a comprehensive security score and fix recommendations.</p>
-              </div>
-            </div>
-          </div>
+          <h2 class="ls-sm fw-bold text-white fs-40px">CA: {{addressToCopy}}</h2>
+          <div class="btn btn-lg bg-green py-1 px-3 rounded fs-24px 14px" @click="copyToClipboard(addressToCopy)"><b>Click Here to Copy CA!</b></div>
         </div>
-
+        <div class="hero-text mb-5">
+          <h2 class="ls-sm fw-bold text-white fs-40px">Get Started (BETA Version)</h2>
+          <div class="row gy-4 justify-content-center" data-aos="fade-right" data-aos-delay="100">
+            <div class="col-lg-6">
+              <div class="card rounded-sm feature-card h-100">
+                <div class="card-body text-start">
+                  <h6 class="text-white">1. Sign Up</h6>
+                  <p class="text-light fw-normal fs-13px m-0">Create your account and connect your Web3 wallet.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="card rounded-sm feature-card h-100">
+                <div class="card-body text-start">
+                  <h6 class="text-white">2. Run a Basic Scan through our AI chat box</h6>
+                  <p class="text-light fw-normal fs-13px m-0">Test your smart contract for free.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="card rounded-sm feature-card h-100">
+                <div class="card-body text-start">
+                  <h6 class="text-white">3. Unlock Advanced Features</h6>
+                  <p class="text-light fw-normal fs-13px m-0">Stake your tokens for premium audits.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="card rounded-sm feature-card h-100">
+                <div class="card-body text-start">
+                  <h6 class="text-white">4. Receive Security Reports</h6>
+                  <p class="text-light fw-normal fs-13px m-0">Get a comprehensive security score and fix recommendations.</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="hero-prompt mb-4">
           <div class="col-12 col-lg-8 m-auto">
