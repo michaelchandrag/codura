81423459ca0de4a17fdbab1bce035c51d712c135
Poosdim <poosdim@gmail.com>
2025-01-22 16:38:16 +0700

    up logo

--- AEGISIX/aegesix.front.master/src/components/Header.vue
+++ AEGISIX/aegesix.front.master/src/components/Header.vue
@@ -17,7 +17,7 @@ const changeMenu = (menu) => {
   <header id="header" class="header d-flex align-items-center fixed-top">
     <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
       <router-link @click.prevent="changeMenu('home')" :to="{ name: 'home' }" class="logo d-flex align-items-center">
-        <img src="/assets/img/aegesix.png" class="lg" alt="aegesix" />
+        <img src="/assets/img/aegesix-lg.png" class="lg" alt="aegesix" />
         <!-- <h1 class="sitename">aegesix</h1> -->
       </router-link>
       <nav id="navmenu-center" class="navmenu ps-6">
